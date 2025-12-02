4bd19c938f02d4c2fbc2dc486f47e4096ebd9925
student0795 <student.0795@gmail.com>
2025-12-02 14:09:20 +0530

    Update DF.py

--- DF.py
+++ DF.py
@@ -6,3 +6,14 @@ print(f"Expression: {expr}")
 a = ((x+h)**3 - x**3)/h 
 limit_expr = limit(a, h, 0) 
 print(f"f`({expr}) = {limit_expr}") 
+
+
+from sympy import * 
+x = symbols('x') 
+h = symbols('h') 
+#Exp 
+expr = 4 - x**2 
+print(f"Expressions: {expr}") 
+a = (4 - ((x+h)**2) - (4-(x**2)))/h 
+limit_expr = limit(a, h, 0) 
+print(f"f' ({expr}) = {limit_expr}")
